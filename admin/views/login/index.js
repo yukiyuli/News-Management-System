98b649585e4841b0a88edd3c4147cfb77b52fa7e
yukiyuli <yukiyuli6688@gmail.com>
2023-03-03 16:17:50 -0500

    add login view

---
+++ admin/views/login/index.js
@@ -0,0 +1,19 @@
+const loginform = document.querySelector("#loginform")
+loginform.onsubmit = async function (evt) {
+    loginwarning.style.display = "none"
+    evt.preventDefault()
+
+    console.log(username.value, password.value)
+
+    // json-server: get=search, post=add, put=update, delete=delete
+
+    let res = await fetch(`http://localhost:3000/users?username=${username.value}&password=${password.value}`).then(res=>res.json())
+
+    if (res.length > 0) {
+        location.href = "/admin/views/home/index.html"
+    } else {
+        console.log("failed")
+        loginwarning.style.display = "block"
+    }
+
+}

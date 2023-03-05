bdbc79ba74ce492e646c53c207656bde97c413fe
yukiyuli <yukiyuli6688@gmail.com>
2023-03-04 19:34:52 -0500

    add top bar

--- admin/util/LoadView.js
+++ admin/util/LoadView.js
@@ -1,9 +1,36 @@
-if (localStorage.getItem("token")) {
-    fetch("/admin/components/topbar/index.html").then(res => res.text()).then(res => {
-        document.querySelector(".topbar").innerHTML = res;
-    })
-
-} else {
-    location.href = "/admin/views/login/index.html"
+function isLogin() {
+    return localStorage.getItem("token")
 }
+
+function renderTopbar(user) {
+    let photo = document.querySelector("#topbar-photo")
+    let currentUsername = document.querySelector("#currentUsername")
+    let exit = document.querySelector("#exit")
+
+    photo.src = user.photo
+    currentUsername.innerHTML = user.username
+
+    exit.onclick = function () {
+        localStorage.removeItem("token")
+        location.href = "/admin/views/login/index.html"
+    }
+}
+
+async function load() {
+    let user = isLogin()
+    if (user) {
+        let topbarText = await fetch("/admin/components/topbar/index.html").then(res => res.text())
+
+        document.querySelector(".topbar").innerHTML = topbarText;
+
+        renderTopbar(JSON.parse(user))
+    }
+    else {
+        location.href = "/admin/views/login/index.html"
+    }
+}
+
+export { load }
+
+
 

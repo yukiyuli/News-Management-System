ad533c67732f69feeb6e5094a58a444b9e09f0ed
yukiyuli <yukiyuli6688@gmail.com>
2023-03-31 20:51:30 -0400

    change echarts data

--- admin/views/home/index.js
+++ admin/views/home/index.js
@@ -7,6 +7,8 @@ load("sidemenu-home")
 
 let user = JSON.parse(isLogin())
 
+let categoryList = ["Lastest news", "Typical Case", "Announcement"]
+
 document.querySelector(".userprofile").innerHTML = `
     <img src="${user.photo}" style="width:100px;" />
     <div>
@@ -18,49 +20,62 @@ document.querySelector(".userprofile").innerHTML = `
 
 // pre: keep the original text style
 
-// Initialize the echarts instance based on the prepared dom
-var myChart = echarts.init(document.getElementById('main'));
 
-// Specify the configuration items and data for the chart
-var option = option = {
-    title: {
-        text: 'News posted by current user',
-        subtext: 'Proportion of different categories',
-        left: 'center'
-    },
-    tooltip: {
-        trigger: 'item'
-    },
-    legend: {
-        orient: 'vertical',
-        left: 'left'
-    },
-    series: [
-        {
-            name: 'Access From',
-            type: 'pie',
-            radius: '50%',
-            data: [
-                { value: 1048, name: 'Search Engine' },
-                { value: 735, name: 'Direct' },
-                { value: 580, name: 'Email' },
-                { value: 484, name: 'Union Ads' },
-                { value: 300, name: 'Video Ads' }
-            ],
-            emphasis: {
-                itemStyle: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+
+
+
+async function analyst() {
+    let res = await fetch("http://localhost:3000/news?author=" + user.username).then(res => res.json())
+
+    let obj = _.groupBy(res, item => item.category)
+
+    let arr = []
+    for (let i in obj) {
+        arr.push({
+            name: categoryList[i],
+            value: obj[i].length
+        })
+    }
+    renderEcharts(arr)
+}
+
+function renderEcharts(data) {
+    // Initialize the echarts instance based on the prepared dom
+    var myChart = echarts.init(document.getElementById('main'));
+
+    // Specify the configuration items and data for the chart
+    var option = option = {
+        title: {
+            text: 'News posted by current user',
+            subtext: 'Proportion of different categories',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
+        series: [
+            {
+                name: 'Category',
+                type: 'pie',
+                radius: '50%',
+                data: data,
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
                 }
             }
-        }
-    ]
-};
+        ]
+    };
 
-// Display the chart using the configuration items and data just specified.
-myChart.setOption(option);
-
-async function analyist(){
-    let res = await fetch("http://localhost:3000/news?author="+user.username).then(res=>res.json())
+    // Display the chart using the configuration items and data just specified.
+    myChart.setOption(option);
 }
+
+analyst()

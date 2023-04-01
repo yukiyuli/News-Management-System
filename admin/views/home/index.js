63c4fd8f0f20ce65933a98966ddec4f7863c0e42
yukiyuli <yukiyuli6688@gmail.com>
2023-03-31 20:21:26 -0400

    add echarts

--- admin/views/home/index.js
+++ admin/views/home/index.js
@@ -17,3 +17,31 @@ document.querySelector(".userprofile").innerHTML = `
 `
 
 // pre: keep the original text style
+
+  // Initialize the echarts instance based on the prepared dom
+  var myChart = echarts.init(document.getElementById('main'));
+
+  // Specify the configuration items and data for the chart
+  var option = {
+    title: {
+      text: 'ECharts Getting Started Example'
+    },
+    tooltip: {},
+    legend: {
+      data: ['sales']
+    },
+    xAxis: {
+      data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks']
+    },
+    yAxis: {},
+    series: [
+      {
+        name: 'sales',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }
+    ]
+  };
+
+  // Display the chart using the configuration items and data just specified.
+  myChart.setOption(option);

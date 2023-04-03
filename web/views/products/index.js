991c70f2f2673af404ff13bc455d4d19fd49d841
yukiyuli <yukiyuli6688@gmail.com>
2023-04-03 17:54:28 -0400

    add dynamically effects

--- web/views/products/index.js
+++ web/views/products/index.js
@@ -2,6 +2,37 @@
 
 import { load } from "/web/util/LoadView.js"
 
-load( "topbar-products" )
+load("topbar-products")
 
 
+async function render() {
+    let list = await fetch("http://localhost:3000/products").then(res => res.json())
+
+    // Dynamically create indicators
+    let indicatorHTML = document.querySelector(".carousel-indicators")
+    indicatorHTML.innerHTML = list.map((item, index) => `
+        <button type="button" 
+        data-bs-target="#carouselExampleCaptions" 
+        data-bs-slide-to="{index}" 
+        class="active" 
+        aria-current="true" 
+        aria-label="$(item.title}">
+        </button>
+    `).join("")
+
+    // Dynamically create slides
+    let innerCarouselHtml = document.querySelector(".carousel-inner")
+    innerCarouselHtml.innerHTML = list.map((item, index) => `
+        <div class="carousel-item ${index === 0 ? "active" : ""}"> 
+            <div
+            style="background-image: url(${item.cover});width:100%;height: calc(100vh - 50px);background-size: cover;">
+            </div>
+            <div class="carousel-caption d-none d-md-block">
+            <h5>${item.title}</title></h5>
+            <p>${item.introduction}</p>
+            </div>
+        </div>
+    `).join("")
+}
+
+render()

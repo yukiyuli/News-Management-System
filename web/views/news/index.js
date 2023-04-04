b7682e680049c689fb7eb1df19c4c6af996004f7
yukiyuli <yukiyuli6688@gmail.com>
2023-04-04 17:05:05 -0400

    add search function

--- web/views/news/index.js
+++ web/views/news/index.js
@@ -2,6 +2,19 @@
 
 import { load } from "/web/util/LoadView.js"
 
-load( "topbar-news" )
+load("topbar-news")
 
+search.oninput =async function(){
+
+    if(!search.value ) {
+        document.querySelector(".list-group").style.display = "none"   // 隐藏搜索结果框
+        return
+    } 
+    document.querySelector(".list-group").style.display = "block"   // 显示搜索结果框
+     let res =await fetch("http://localhost:3000/news?title_like="+search.value).then(res=>res.json())
+    // console.log(res)
+    document.querySelector(".list-group").innerHTML = res.map(item=>`
+    <li class="list-group-item">${item.title}</a></li>
+    `).join("")
+}
 
